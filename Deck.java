59c1f9865ba5d3438fb88c51bae10660dfe767c9
Aidan <adavis@brooksschool.org>
2026-02-16 12:06:23 -0500

    draw card simpler, player score gonn be in player

--- Deck.java
+++ Deck.java
@@ -1,39 +1,55 @@
+import java.util.Random;
+
 public class Deck {
     private static int[] deck = new int[55];
-    private static int index = 0;
+    private int length = deck.length;
+    private Random random = new Random();
 
-    for (int num = 1; num <= 10; num++){
-        for (int count = 0; count < num; count++){
-            deck[index] = num;
-            index++;
+    public Deck(){
+        int index = 0;
+        for (int num = 1; num <= 10; num++){
+            for (int count = 0; count < num; count++){
+                deck[index] = num;
+                index++;
+            }
         }
     }
 
-    public static int drawCard(){
-        int card = deck[0];
-        boolean same = false;
-        for (int j = 0; j < handIndex; j++){
-            if (hand[j] == card){
-                playerScore += card;
-                same = true;
-                break;
-            }
+
+
+    public void shuffle(){
+        for (i = length - 1; i > 0; i--){
+            int ran = random.nextInt(i + 1);//this random is different but when I searched the shuffle method this was what popped up
+            int temp = deck[i];
+            deck[i] = deck[ran];
+            deck[ran] = deck[i];
+
         }
-        if (!same && handIndex < hand.length) {
-            hand[handIndex] = card;
-            handIndex++;
+    }
+
+    public int drawCard(){
+        boolean isCards = true;
+        if (length == 0){
+            isCards = false;
+            return isCards;
         }
-        removeTopCard();
+        int card = deck(length - 1);
+        length--;
         return card;
     }
 
-    public static void removeTopCard(){
+    /*
+       public static void removeTopCard(){
         int[] newDeck = new int[deck.length - 1];
         for (int i = 1; i < deck.length; i++){
             newDeck[i - 1] = deck[i];
         }
         deck = newDeck;
     }
+     */
+ 
+    // use fisher-yates algorithm for shuffle
+    // pull last card instead so don't have to make a new array
 
-    public Deck();
+    
 }
--- Player.java
+++ Player.java
@@ -1,5 +1,31 @@
 public class Player{
-    private int[] hand = new int[10];
+    private int[] hand = new int[0];
+    private int handsize = 0;
     private int playerScore = 0;
-    private int handIndex = 0;
+
+    player.takeCard(int card){
+        if (hand.contains(card)){
+            score += card;
+        }
+    }
+
+    //use this for later:
+    /*
+        boolean same = false;
+    for (int j = 0; j < handIndex; j++){
+        if (hand[j] == card){
+            playerScore += card;
+            same = true;
+            break;
+        }
+    }
+    if (!same && handIndex < hand.length) {
+        hand[handIndex] = card;
+        handIndex++;
+    }
+     */
+
+
+    // add a take card function for players
+    // make a player array for all the players
 }

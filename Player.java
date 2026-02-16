cafdfcabdcb4361564b1a8c5fea4ec1775f61910
Aidan <adavis@brooksschool.org>
2026-02-16 12:13:53 -0500

    take card

--- Player.java
+++ Player.java
@@ -1,29 +1,23 @@
 public class Player{
     private int[] hand = new int[0];
-    private int handsize = 0;
+    private int handSize = 0;
     private int playerScore = 0;
 
-    player.takeCard(int card){
-        if (hand.contains(card)){
-            score += card;
+    public void takeCard (int card){
+        for (int i = 0; i < handSize){
+            if (hand[i] == card){
+                score += card;
+                handSize = 0;
+                return;
+            }
         }
+
+        hand[handSize] = card;
+        handSize++;
     }
 
-    //use this for later:
-    /*
-        boolean same = false;
-    for (int j = 0; j < handIndex; j++){
-        if (hand[j] == card){
-            playerScore += card;
-            same = true;
-            break;
-        }
-    }
-    if (!same && handIndex < hand.length) {
-        hand[handIndex] = card;
-        handIndex++;
-    }
-     */
+
+
 
 
     // add a take card function for players
